f51e6ef2b851d29e7b05c989c775d779a3744f73
anitaneza <chachaanita.nd@gmail.com>
2026-05-28 20:50:08 +0700

    ubah grafis js

--- js/grafik.js
+++ js/grafik.js
@@ -8,6 +8,14 @@ const Grafik = (() => {
     const monthly = await API.getMonthly();
     Charts.loadMonthly(monthly);
   }
+  
+  async function loadGrafikTab(range) {
+  const [sensor, esp2] = await Promise.all([
+    API.getESP1(range),
+    API.getESP2(range)
+  ]);
+  Charts.loadGrafik(sensor, esp2);
+  }
 
   return { load };
 })();
@@ -15,19 +23,7 @@ const Grafik = (() => {
 function setFilter(range, btn) {
   document.querySelectorAll(".filter-btn").forEach(b => b.classList.remove("active"));
   btn.classList.add("active");
+  loadGrafikTab(range === "today" ? "daily" : range === "7d" ? "weekly" : "monthly");
   Grafik.load(range);
 }
 
-async function loadGrafikTab(range) {
-  const [sensor, esp2] = await Promise.all([
-    API.getESP1(range),
-    API.getESP2(range)
-  ]);
-  Charts.loadGrafik(sensor, esp2);
-}
-
-function setFilter(range, btn) {
-  document.querySelectorAll(".filter-btn").forEach(b => b.classList.remove("active"));
-  btn.classList.add("active");
-  loadGrafikTab(range === "today" ? "daily" : range === "7d" ? "weekly" : "monthly");
-}
